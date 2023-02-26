3251adffac1e509c915625935a3bffc4d343b0c0
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-02-26 10:45:33 -0600

    add photosindex function

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,12 @@
+import { PhotosIndex } from "./PhotosIndex";
 export function Content() {
+  const photos = [
+    { id: 1, name: "Fist", url: "https://via.placeholder.com/150", width: 150, height: 150 },
+    { id: 2, name: "second", url: "https://via.placeholder.com/300", width: 300, height: 300 },
+  ];
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <PhotosIndex photos={photos} />
     </div>
   );
 }
